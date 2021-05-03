c654bf2b23e99b888f7506077eaae2d300d96f0a
ahmadXtn <Ahmed.Bouazizi@issatmh.u-monastir.tn>
2021-05-03 06:46:17 +0200

    wkhtmltopdf vs html-pdf-node

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const srcList = require('./utils/srcPages');
 const html_to_pdf = require('html-pdf-node');
 const fs = require('fs');
-
-
-
-
-
-
-
 const util = require('util');
+
 const exec = util.promisify(require('child_process').exec);
+const file = {url: "https://storage.libmanuels.fr/Delagrave/specimen/9782206307909/3/OEBPS/page044.xhtml"};
+
+
+
+html_to_pdf.generatePdf(file,
+	{width: '2048', height: '2048'})
+	.then(pdfBuffer => {
+		console.log("PDF Buffer:-", pdfBuffer);
+		fs.writeFileSync("html_to_pdf_output.pdf", pdfBuffer);
+	});
+
+
 
 async function lsExample() {
-	const { stdout, stderr } = await exec('wkhtmltopdf https://storage.libmanuels.fr/Delagrave/specimen/9782206307909/3/OEBPS/page044.xhtml out.pdf');
+	const { stdout, stderr } = await exec(`wkhtmltopdf ${file.url} wkhtmltopdf_output.pdf`);
 	console.log('stdout:', stdout);
 	console.error('stderr:', stderr);
 }
