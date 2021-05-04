5ea26e1b1bbd4853edc1d6ffccb19dbd912e3686
ahmadXtn <Ahmed.Bouazizi@issatmh.u-monastir.tn>
2021-05-04 19:12:09 +0200

    feat:node-wkhtmltopdf

--- index.js
+++ index.js
@@ -1,31 +1,23 @@
-const srcList = require('./utils/srcPages');
-const html_to_pdf = require('html-pdf-node');
-const fs = require('fs');
-const util = require('util');
-const path = require("path");
-
-const exec = util.promisify(require('child_process').exec);
-const file = {url: "https://storage.libmanuels.fr/Delagrave/specimen/9782206307909/3/OEBPS/page044.xhtml"};
-
-
-
-
-
-html_to_pdf.generatePdf(file,
-	{width: '2048', height: '2048'})
-	.then(pdfBuffer => {
-		console.log("PDF Buffer:-", pdfBuffer);
-		fs.writeFileSync("download/html_to_pdf_output.pdf", pdfBuffer);
-	});
-
-
-
-
-async function lsExample() {
-	const { stdout, stderr } = await exec(`wkhtmltopdf ${file.url} wkhtmltopdf_output.pdf`,{
-		cwd:path.resolve('download')
-	});
-	console.log('stdout:', stdout);
-	console.error('stderr:', stderr);
+'use strict';
+const wkhtmltopdf = require('wkhtmltopdf');
+const axios = require('axios').default;
+const jsonData = 'https://storage.libmanuels.fr/Delagrave/specimen/9782206307909/3/META-INF/interactives.json';
+const file = {
+	baseURLrl: 'https://storage.libmanuels.fr/Delagrave/specimen/9782206307909/3/OEBPS/'
 }
-lsExample();
+axios.get(jsonData, {})
+	.then((res) => {
+		let page = res.data.pages.page;
+		let pageIterator = page.values();
+		let result = pageIterator.next();
+
+		while (!result.done) {
+			let src = result.value.attributes.src;
+			wkhtmltopdf(`${file.baseURLrl}${src}`, {
+				output: `download/9782206307909/${src.substring(0, src.length - 5)}pdf`,
+				"s": "A3"
+			});
+			result = pageIterator.next();
+		}
+	})
+
