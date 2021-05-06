fddd9547858d06b4458f07db1fdee3ba4396e9a7
ahmadXtn <Ahmed.Bouazizi@issatmh.u-monastir.tn>
2021-05-06 22:27:10 +0200

    test:processing css output with autoprefixer plugin

--- index.js
+++ index.js
@@ -1,65 +1,102 @@
 'use strict';
 const util = require('util');
-const execSync = util.promisify(require('child_process').execSync);
+const execSync = require('child_process').execSync;
 const exec = util.promisify(require('child_process').exec);
-const axios = require('axios').default;
 const path = require('path');
-const autoprefixer = require('autoprefixer')
-const postcss = require('postcss')
-const fs = require("fs");
+const autoprefixer = require('autoprefixer');
+const postcss = require('postcss');
+const https = require('https');
+const {exists, writeFileSync} = require("fs");
 
 
-const jsonUrl = 'https://storage.libmanuels.fr/Delagrave/specimen/9782206309071/1/META-INF/interactives.json';
 const file = {
-	baseUrl: 'https://storage.libmanuels.fr/Delagrave/specimen/9782206309071/1/OEBPS/',
-	cssBaseUrl: 'https://storage.libmanuels.fr/Delagrave/specimen/9782206309071/1/OEBPS/css/cover.css'
+	htmlBaseUrl: 'https://storage.libmanuels.fr/Delagrave/specimen/9782206309071/1/OEBPS/',
+	cssBaseUrl: 'https://storage.libmanuels.fr/Delagrave/specimen/9782206309071/1/OEBPS/css/'
+}
+const options = new URL('https://storage.libmanuels.fr/Delagrave/specimen/9782206309071/1/META-INF/interactives.json');
+
+let targetCss = path.resolve(`css/out.css`)
+
+
+https.get(options, (resp) => {
+	let data = '';
+	// A chunk of data has been received.
+	resp.on('data', (chunk) => {
+		data += chunk.toString();
+	});
+	// The whole response has been received. Print out the result.
+	resp.on('end', () => {
+		let pages = JSON.parse(data).pages;
+		let page = pages.page;
+		let pageIterator = page.values();
+		let result = pageIterator.next();
+		for (let elm of page) {
+			let src = elm.attributes.src;
+			let styleRef = src.split('.')[0].replace(/[^0-9]/g, '');
+			let cssTargetUrl = `${file.cssBaseUrl}Styles${styleRef}.css`;
+
+			if (styleRef === "") {
+				styleRef = "cover"
+				cssTargetUrl = `${file.cssBaseUrl}${styleRef}.css`;
+			}
+			getStyleSheet(cssTargetUrl, styleRef, src);
+
+		}
+		/*
+		while (!result.done) {
+			let src = result.value.attributes.src;
+			let styleRef = src.split('.')[0].replace(/[^0-9]/g, '');
+			let targetUrl = `${file.cssBaseUrl}Styles${styleRef}.css`;
+
+			if (styleRef === "") {
+				styleRef = "cover"
+				targetUrl = `${file.cssBaseUrl}${styleRef}.css`;
+			}
+
+
+			result = pageIterator.next();
+		}*/
+	});
+}).on("error", (err) => {
+	console.log("Error: de" + err.message);
+});
+
+ function getStyleSheet(url, styleRef, src) {
+	const options = new URL(url);
+	const cssPath = path.resolve(`css/${styleRef}.css`);
+	let htmlTargetUrl = `${file.htmlBaseUrl}${src}`;
+
+	https.get(options, (resp) => {
+		let data = '';
+		// A chunk of data has been received.
+		resp.on('data', (chunk) => {
+			data += chunk.toString();
+		});
+		// The whole response has been received. Print out the result.
+		resp.on('end', async (path, callback) => {
+			await postcss([
+				autoprefixer({overrideBrowserslist: ['last 1 version']})])
+				.process(data, {
+					from: undefined
+				})
+				.then(result => {
+					try {
+						writeFileSync(`css/${styleRef}.css`, result.css, {});
+					}catch (e){console.log(e)}
+					console.log("css processing done!");
+					//execSync(`wkhtmltopdf --user-style-sheet ${cssPath}  -s A3  ${htmlTargetUrl} download/9782206307909/${styleRef}.pdf`);
+				})
+				.catch(err => console.log('failed to load style'));
+		});
+	}).on("error", (err) => {
+		console.log("Error: de" + err.message);
+	});
+
 }
 
 
 
+async function processCss(cssUrl) {
 
-axios.get(jsonUrl, {})
-	.then((res) => {
-		let page = res.data.pages.page;
-		let pageIterator = page.values();
-		let result = pageIterator.next();
-
-		while (!result.done) {
-			let src = result.value.attributes.src;
-			let styleRef = "Styles".concat(src.split('.')[0].replace(/[^0-9]/g, ''));
-			if (styleRef === "Styles") {
-				execSync(`wkhtmltopdf  -s A3  ${file.baseUrl}${src} download/9782206307909/${src.substring(0, src.length - 5)}pdf`);
-			}
-			axios.get(`https://storage.libmanuels.fr/Delagrave/specimen/9782206309071/1/OEBPS/css/${styleRef}.css`, {})
-				.then((res) => {
-					postcss([
-						autoprefixer({overrideBrowserslist: ['last 1 version']})])
-						.process(res.data, {
-							from: undefined
-						})
-						.then(result => {
-							fs.writeFileSync(`css/${styleRef}.css`, result.css,(err)=>{
-								if (err) console.log(err)
-							});
-							const customStyleSheet = path.resolve(`css/${styleRef}.css`);
-							execSync(`wkhtmltopdf --user-style-sheet ${customStyleSheet}  -s A3  ${file.baseUrl}${src} download/9782206307909/${src.substring(0, src.length - 5)}pdf`);
-						})
-						.catch(err=>console.log('failed to load style'))
-				});
-
-
-			/**
-			 * High Memory Usage
-			 * Multiple wkhtmltopdf instance Running Simultaneously
-			const { stdout, stderr } =  exec(`wkhtmltopdf --page-size A3  ${file.baseURLrl}${src} download/9782206307909/${src.substring(0, src.length - 5)}pdf`);
-			 */
-			result = pageIterator.next();
-		}
-
-	});
-
-
-function getJsonData() {
-	return axios.get(jsonUrl);
 }
 
